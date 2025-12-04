a32a7ad0e101afdccd7ea55b9cd92433df0b01e4
RaymussenArthur <raymussenaw@gmail.com>
2025-12-04 20:27:01 +0700

    Commit No. 4 – Assignment

--- LAB_WEEK_12/app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
+++ LAB_WEEK_12/app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.test_lab_week_12
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.RecyclerView
 import com.example.test_lab_week_12.model.Movie
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
@@ -31,20 +35,13 @@ class MainActivity : AppCompatActivity() {
         val movieViewModel = ViewModelProvider(
             this, object : ViewModelProvider.Factory {
                 override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                    return MovieViewModel(movieRepository) as T
+                    if (modelClass.isAssignableFrom(MovieViewModel::class.java)) {
+                        @Suppress("UNCHECKED_CAST")
+                        return MovieViewModel(movieRepository) as T
+                    }
+                    throw IllegalArgumentException("Unknown ViewModel class")
                 }
             })[MovieViewModel::class.java]
-        movieViewModel.popularMovies.observe(this) { popularMovies ->
-            val currentYear =
-                Calendar.getInstance().get(Calendar.YEAR).toString()
-            movieAdapter.addMovies(
-                popularMovies
-                    .filter { movie ->
-                        movie.releaseDate?.startsWith(currentYear) == true
-                    }
-                    .sortedByDescending { it.popularity }
-            )
-        }
 
         lifecycleScope.launch {
             // repeatOnLifecycle is a lifecycle-aware coroutine builder
@@ -53,26 +50,32 @@ class MainActivity : AppCompatActivity() {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 launch {
                     // collect the list of movies from the StateFlow
-                    movieViewModel.popularMovies.collect {
+                    movieViewModel.popularMovies.collect { movies ->
+                        val currentYear = Calendar.getInstance().get(Calendar.YEAR).toString()
                         // add the list of movies to the adapter
-                            movies ->
-                        movieAdapter.addMovies(movies)
+                        movieAdapter.addMovies(
+                            movies
+                                .filter { movie ->
+                                    movie.releaseDate?.startsWith(currentYear) == true
+                                }
+                                .sortedByDescending { it.popularity }
+                        )
                     }
                 }
                 launch {
                     // collect the error message from the StateFlow
                     movieViewModel.error.collect { error ->
                         // if an error occurs, show a Snackbar with the error
-                        message
-                        if (error.isNotEmpty()) Snackbar
-                            .make(
+                        if (error.isNotEmpty()) {
+                            Snackbar.make(
                                 recyclerView, error, Snackbar.LENGTH_LONG
                             ).show()
+                        }
                     }
                 }
             }
         }
-        }
+    }
 
     private fun openMovieDetails(movie: Movie) {
         val intent = Intent(this, DetailsActivity::class.java).apply {
